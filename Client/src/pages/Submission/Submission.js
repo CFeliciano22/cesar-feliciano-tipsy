3f07a97bded10abbb60dd27e561be64df3b9691d
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-04-06 12:29:45 -0400

    added functionality to submission page

--- Client/src/pages/Submission/Submission.js
+++ Client/src/pages/Submission/Submission.js
@@ -2,12 +2,45 @@ import './Submission.scss'
 import Rating from '../../Components/Rating/Rating';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
-export default function Submission(){
+import { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+// make axios post request to server to create a new submission
+
+const url = 'http://localhost:5000/restaurants/';
+
+
+export default class Submission extends Component {
+    
+        state = {
+            submit: false
+        }
+    
+    
+        submitEvent = (e) =>{
+            e.preventDefault();
+            const name = e.target.name.value;
+            const display_phone = e.target.phone.value;
+            const happyhour = e.target.times.value;
+            if(name && display_phone && happyhour){
+                alert('Restaurant was uploaded successfully!');
+                axios.post(url, {name, display_phone, happyhour})
+                this.setState({submit:true})
+            }else {
+                alert('Please fill out details!')
+            }
+        }
+        
+        render(){
+            if ( this.state.submit){
+                return <Redirect to='/'/>;
+            }
+     
     return(
         <div className='submission-container'>
         <Header/>
         <div className='submission'>
-            <form className='submission__form'>
+            <form className='submission__form' onSubmit={this.submitEvent}>
                 <h3 className='submission__title'>Restaurant Submissions</h3>
                 <label for='name'>Restaurant Name </label>
                 <input type='text' name='name' />
@@ -15,7 +48,7 @@ export default function Submission(){
                 <input type='text' name='times' />
                 <label for='address'>Address </label>
                 <input type='text' name='address' />
-                <label for='times'>Phone</label>
+                <label for='phone'>Phone</label>
                 <input type='text' name='phone' />
                 <div className='submission__rating-container'>
                 <p className='submission__rating'>Drinks<Rating/></p>
@@ -28,4 +61,4 @@ export default function Submission(){
         <Footer/>
         </div>
     )
-}
+        }}
